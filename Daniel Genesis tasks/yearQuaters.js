770bb07ff2ed5eb4ed8bdc81c42ed94775fa209b
DanielGenesis <Danielokp328@gmail.com>
2025-08-07 12:37:25 +0100

    Refactor quarter determination logic
    Simplified the code for determining which quarter a month belongs to by replacing the loop with direct conditional checks and correcting the month array syntax. Also updated the month variable and improved output formatting.

--- Daniel Genesis tasks/yearQuaters.js
+++ Daniel Genesis tasks/yearQuaters.js
@@ -1,18 +1,13 @@
-const month_in_year = {"jan", "feb", "mar", "apr", "may", "jun", "jul", "aug", "sep", "oct", "nov", "dec"};
-let count = 0;
-let month = 3;
-for (count = 0; count <= month_in_year.length; count++){
-if (month <= 4 && count == (month - 1)) {
-console.log("month " + month + " " + month_in_year[count] + " is part of the first quater");
-}
-if (month <= 8 && month > 4  && count == (month - 1)) {
-console.log("month " + month + " " + month_in_year[count] + " is part of the second quater");
-}
-if (month <= 12 && month > 8  && count == (month - 1)) {
-console.log
-("month " + month + " " + month_in_year[count] + " is part of the third quater");
-}
-}
-}
-
-}
+const month_in_year = ["jan", "feb", "mar", "apr", "may", "jun", "jul", "aug", "sep", "oct", "nov", "dec"]
+month = 5;
+if (month < month_in_year.length){
+	if (month <= 4){
+	console.log(`${"month"}, ${month} ${month_in_year[month - 1]} ${"is in the first quater of the year"}` )
+	}
+	if (month <= 8 && month > 4){
+	console.log(`${"month"}, ${month} ${month_in_year[month - 1]} ${"is in the second quater of the year"}` )
+	}
+	if (month <= 12 && month > 8){
+	console.log(`${"month"}, ${month} ${month_in_year[month - 1]} ${"is in the third quater of the year"}` )
+	}
+}	
