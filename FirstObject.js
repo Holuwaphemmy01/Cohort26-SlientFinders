460232de695d07277c33e6d99e1761e9e9b64062
Ejiofor samuel chukwunonso <samuelchukwusom360@gmail.com>
2025-08-07 20:02:43 +0100

    just a demo file

---
+++ FirstObject.js
@@ -0,0 +1,14 @@
+let Samuel = { name : "Chukwunonso",
+		age : 50,
+		Nationality : "Nigeria",
+		
+		
+            }
+
+for (const details in Samuel){
+if (details === "age")
+console.log(`${details}: ${Samuel[details]}`)
+
+}
+
+
