3d5a4fee83bc78aaf702634c6459c3a134a9a61c
Aracdios56 <akinmi044@gmail.com>
2025-08-07 09:56:41 +0100

    Add files via upload

---
+++ TemiT/Day2.js
@@ -0,0 +1,6 @@
+ function texts_toUpperCase(input) {
+console.log(input.toUpperCase());
+}
+let user_input = "The lord is good.";
+texts_toUpperCase(user_input);
+
---
+++ TemiT/Day3.js
@@ -0,0 +1,6 @@
+ function stringToArray(input) {
+ return input.split();
+}
+let input = "I love programming";
+console.log(stringToArray(input));
+
