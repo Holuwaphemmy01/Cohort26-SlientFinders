086bcc0fc003f8dcbb4398c78f11dedc768649f4
Ejiofor samuel chukwunonso <samuelchukwusom360@gmail.com>
2025-08-07 09:49:54 +0100

    comitted

---
+++ loopTriangle.js
@@ -0,0 +1,50 @@
+for (let i = 0; i < 5; i++){
+let line = "  ";
+for (let j = 5; j > i; j--){
+line = line +" "+" ";
+
+}
+console.log(line);
+for (let j = 0; j < i; j++){
+line = line +j+" ";
+
+}
+//console.log(line);
+for (let m = 0; m < i; m++){
+		
+
+line = line + m +" ";
+
+}
+console.log(line);
+}
+
+
+
+for (let i = 0; i < 5; i++){
+let line = "  ";
+for (let j = 0; j < i; j++){
+line = line +" "+" ";
+
+}
+console.log(line);
+for (let j = 5; j > i; j--){
+line = line + j +" ";
+
+}
+//console.log(line);
+for (let m = 5; m > i; m--){
+		
+
+line = line + m +" ";
+
+}
+console.log(line);
+}
+
+
+
+
+
+
+
