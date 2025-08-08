3242c923efe762d53bfe6b503bbe0193963a2422
LegendaryMakaveli <adeyemoakorede93@gmail.com>
2025-08-08 09:17:00 +0100

    guess lover if there are compatible by thier birthday

---
+++ Adeyemo Emmanuel/Day5.js
@@ -0,0 +1,13 @@
+function mumuLover(numberOne, numberTwo) {
+	if (numberOne % 2 == 0 && numberTwo % 2 == 1) {
+		return true;
+
+		}else {
+			return false;
+		}
+	
+	return numberOne, numberTwo
+}
+
+
+console.log(mumuLover(18,5));
