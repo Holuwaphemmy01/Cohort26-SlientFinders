dfa3541beab05972280b65d627740498ed4aa8de
Ejiofor samuel chukwunonso <samuelchukwusom360@gmail.com>
2025-08-08 09:27:06 +0100

    comitted

---
+++ Day4.js
@@ -0,0 +1,33 @@
+function monthsOfTheYear(number){
+
+switch(number){
+
+case 1: 
+case 2:
+case 3:return "its part of the first quarter";
+break;
+
+case 4:
+case 5:
+case 6:return "its part of the Second quarter";
+break;
+case 7:
+case 8:
+case 9: return "its part of the Third quarter";
+break;
+case 10:
+case 11:
+case 12:return "its part of the Fourth quarter";
+
+
+
+}
+
+}
+
+
+console.log(monthsOfTheYear(10));
+
+
+
+
