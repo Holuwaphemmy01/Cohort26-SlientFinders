4276ca9e7137c909a6f1a43c0dc80b09329d2351
Ezenwankwo Emmanuel Somtoo <Somtooezenwankwo@gmail.com>
2025-08-09 14:41:00 +0100

    Added Day3 task

---
+++ EzeEmma/Day3.js
@@ -0,0 +1,8 @@
+function splitToArrayOfWords(input){
+    return input.split(" ");
+}
+
+let test = "I Love You Mimi";
+let testResult = splitToArrayOfWords(test);
+console.log(test);
+console.log(testResult);
