2828f046ccc7a0234909268ebb31f068af598ffc
Ezenwankwo Emmanuel Somtoo <Somtooezenwankwo@gmail.com>
2025-08-09 15:04:07 +0100

    Added Day5 task

--- EzeEmma/Day5.js
+++ EzeEmma/Day5.js
@@ -2,6 +2,6 @@ function inLove(flower1, flower2) {
     return (flower1 % 2 !== flower2 % 2);
 }
 
-console.log(inLove(4, 7));
+console.log(inLove(4, 77));
 console.log(inLove(2, 4));
 console.log(inLove(9, 8));
