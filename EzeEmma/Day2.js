3e0f21bcd07761e1a1b088178c3dba5acff0857c
Ezenwankwo Emmanuel Somtoo <Somtooezenwankwo@gmail.com>
2025-08-09 13:37:02 +0100

    Added Day2 task

---
+++ EzeEmma/Day2.js
@@ -0,0 +1,8 @@
+function convertToUpperString(input){
+    return input.toUpperCase()
+}
+
+
+let name = "my name is ezenwankwo emmanuel.";
+let testResult = convertToUpperString(name);
+console.log(testResult);
