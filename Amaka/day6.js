e76ea7d0666a39d2fda0c760ce72cf31c2a5e7c0
Chiamie <okechukwurchiamaka@gmail.com>
2025-08-09 19:09:27 +0100

    attempt 2

--- Amaka/day6.js
+++ Amaka/day6.js
@@ -1,34 +1,45 @@
 function getAllPermutationOf(aString){
 	let aArray = [];
 	let result;
-	for (let value = 0; value < aString.length; value++){
-		result = aString[value].toUpperCase() + aString.slice(1, 3);
+	if (aString == aString.toLowerCase())
+		aArray.push(aString);
+		result = aString.toUpperCase();
+		console.log(result);
 		aArray.push(result);
-		//console.log(result);
+	for (let value = 0; value < aString.length; value++){
+		console.log(value);
+		if (value == 0){
+			result = aString[value].toUpperCase() + aString.slice(1, 3);
+			aArray.push(result);
+			console.log(result);
+		}else if (value == 1){
+			result = aString.charAt(0) + aString[value].toUpperCase() + aString.slice(2, 3);
+			aArray.push(result);
+			console.log(result);
+		} else if (value == 2){
+			result = aString.slice(0, 2) + aString[value].toUpperCase();
+			aArray.push(result);
+			console.log(result);
+		}
 	}
-	return aArray;
+	for (let value = 0; value < aString.length; value++){
+		console.log(value);
+		if (value == 0){
+			result = aString[value].toUpperCase() + aString[value + 1].toUpperCase() + aString.slice(-1);
+			aArray.push(result);
+			console.log(result);
+		}else if (value == 1){
+			result = aString.charAt(0) + aString[value].toUpperCase() + aString[value + 1].toUpperCase();
+			aArray.push(result);
+			console.log(result);
+		} else if (value == 2){
+			result = aString[value - 1].toUpperCase() + aString.charAt(0) + aString[value].toUpperCase();
+			aArray.push(result);
+			console.log(result);
+		}
+
+	}
+		return aArray;
 }
 let name = "abc";
-console.log(getAllPermutationOf(name))
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+console.log(getAllPermutationOf(name));
