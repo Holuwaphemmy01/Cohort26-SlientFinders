6a81b0ff520baa960442e2cc82de4bddef9d5941
DanielGenesis <Danielokp328@gmail.com>
2025-08-11 00:55:44 +0100

    Refactor quiz game to support multiple questions
    Reworked the quiz game to use arrays for questions and answers, allowing multiple questions to be asked in sequence. Added score tracking for correct and wrong answers, improved input validation, and updated messaging for a better user experience.

--- Daniel Genesis tasks/quizGame.js
+++ Daniel Genesis tasks/quizGame.js
@@ -2,18 +2,49 @@ const readline = require('readline');
 
 const readLineI = readline.createInterface({ input: process.stdin,  output: process.stdout });
 
-console.log("All of this options show current cohorts in Semicolon, except ?");
-console.log("(1). Cohort-27   (2). Cohort-25   (3). Cohort-21   (4). Cohort-23 ");
+console.log("Welcom to QuizMe game, play Responsibly ['_']");
 
+let myQuestionArray =[`Question 1. All of this options show current cohorts in Semicolon, except ?
+(1). Cohort-27   (2). Cohort-25   (3). Cohort-21   (4). Cohort-23 ` ,
+ `Question 2. The following religions exist except ?
+(1). Christian   (2). Islam   (3). Budha   (4). Witchcraft ` , 
+`Question 3. The following are programming languages except ?
+(1). Java   (2). Algorithm   (3). Java script   (4). Python `];
+let correctAnswers = [3, 4, 2];
+let correctAnswersTotal = 0;
+let wrongAnswersTotal = 0;
+let question = 0;
+
+function quizQuestion() {
+if (question < myQuestionArray.length) {
+console.log(myQuestionArray[question]);
 readLineI.question("Select an option from (1 - 4): ", (input) => {let answer = Number(input);
-if (answer == 3){
-console.log("Correct!!! Cohort-22 is not a current cohort.");
+
+if (answer >= 1 && answer <= 4) {
+if (answer == correctAnswers[question]) {
+console.log("Correct!!\/!!.. ");
+correctAnswersTotal++;
 } 
-else if (answer == 1 || answer == 2 || answer == 4){
-console.log("Incorrect !!X!! Cohort-21 is not a current cohort.");
+else {
+console.log("Incorrect !!X!! ");
+wrongAnswersTotal++ ;
+}
 }
 else {
-console.log("Invalid input !!!. Please select an option from the given options");
+console.log("Invalid input !!!. Come back when youre smarter.");
+}
+question++;
+quizQuestion();
+});
+
+}
+else {
+console.log("Quiz Completed!!!");
+console.log("Total Correct answers: ", correctAnswersTotal);
+console.log("Total Wrong answers: ", wrongAnswersTotal);
+readLineI.close();
+}
 }
 
-});
+quizQuestion(); 
+
