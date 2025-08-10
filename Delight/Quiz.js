24fba04e20f1447d23d46c46cb3023c5c5ca41ea
Delight44-max <eboagorojahdiebube@gmail.com>
2025-08-10 12:32:39 +0100

    added new file

---
+++ Delight/Quiz.js
@@ -0,0 +1,36 @@
+function quiz(question) {
+    console.log(question);
+}
+
+let question = "What is the capital of France?";
+
+function get_answers(options, choice) {
+    let correctCount = 0;
+    let incorrectCount = 0;
+
+    console.log("Options:");
+    for (let key in options) {
+        console.log(${key}: ${options[key]});
+    }
+
+    
+    if (choice === 1) {
+        correctCount++;
+        console.log("Correct!");
+    } else {
+        incorrectCount++;
+        console.log("Incorrect!");
+    }
+
+    console.log(Correct: ${correctCount}, Incorrect: ${incorrectCount});
+}
+
+let options = {
+    1: "Paris",
+    2: "London",
+    3: "Berlin",
+    4: "Rome"
+};
+
+quiz(question);
+get_answers(options, 1);
