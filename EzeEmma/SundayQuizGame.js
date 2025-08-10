01ae0a96936bd6d7c181371213a5ffa62bba2133
Ezenwankwo Emmanuel Somtoo <Somtooezenwankwo@gmail.com>
2025-08-10 15:57:06 +0100

    Attempting Sunday Quiz

---
+++ EzeEmma/SundayQuizGame.js
@@ -0,0 +1,8 @@
+const myQuizQuestions = [
+    {Question: "What is my name?", Options: ["Ezen", "Emmanuel", "Sopulu", "Gabriel"], Answer:"Emmanuel"},
+    {Question: "What is my surname?", Options: ["Ezen", "Emmanuel", "Sopulu", "Gabriel"], Answer:"Emmanuel"},
+    {Question: "What is my occupation?", Options: ["Ezen", "Emmanuel", "Sopulu", "Gabriel"], Answer:"Emmanuel"},
+    {Question: "What is my title?", Options: ["Ezen", "Emmanuel", "Sopulu", "Gabriel"], Answer:"Emmanuel"},
+    {Question: "What is my relationship status?", Options: ["Ezen", "Emmanuel", "Sopulu", "Gabriel"], Answer:"Emmanuel"},
+   
+]
