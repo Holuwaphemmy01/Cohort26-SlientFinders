ecfea75e9c75665e9855a128c16785ea0e725ae2
EngrEOOnoja <onojaemmanuel193@gmail.com>
2025-08-10 15:46:20 +0100

    little changes

--- OnojaEmmanuelJS/GuessingGame.js
+++ OnojaEmmanuelJS/GuessingGame.js
@@ -1,154 +1,127 @@
-function isGuessingGame(quest1){
-	const option  = `
-		
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+let presentQuestion = 1;
+let correct = 0
+let incorrect = 0
+
+
+function askQuestion() {
+  switch (presentQuestion) {
+    case 1:
+      console.log(`
 A. Which tech giant recently reached a $4 trillion valuation?
-	1. Apple	
-	2. Google
-	3. Microsoft
-	4. Amazon
-	5. Next
-	6. Exit
+  1. Apple
+  2. Google
+  3. Microsoft
+  4. Amazon
+`);
+      rl.question("Your answer: ", (answer) => {
+        if (parseInt(answer) === 3) {
+          console.log(" Correct!. Move to the next");
+          correct += 1;
+        } else {
+        	incorrect += 1;
+          console.log(" Incorrect. Move to the next");
+        }
+        presentQuestion++;
+        askQuestion();
+      });
+      break;
 
-	`;
-	
+    case 2:
+      console.log(`
+2. Which company won a $10 billion U.S. government contract for data analytics?
+  1. IBM
+  2. Palantir
+  3. Oracle
+  4. Cisco
+`);
+      rl.question("Your answer: ", (answer) => {
+        if (parseInt(answer) === 2) {
+        correct += 1
+          console.log(" Correct!.  Move to the next");
+        } else {
+        	incorrect += 1
+          console.log(" Incorrect. Move to the next");
+        }
+       presentQuestion++;
+        askQuestion();
+      });
+      break;
 
-//✅ Answer: C) Microsoft
-console.log(option);
-	switch(quest1){
-	case 1: console.log("incorrect isGuessingGame2(quest1)"); break;
-	case 2: console.log("2 Is incorrect, \n move to the next question" ); break;
-	case 3: console.log("3 Is correct, \n move to the next question" ); break;
-	case 4: console.log("4 Is incorrect, \n move to the next question" ); break;
-	case 5: isGuessingGame(quest1); break
-	case 6 :
-		default : console.log(" Not a correct input" ); 
-	}
-return option;
-}
-
-console.log( isGuessingGame(5));
-
-
-
-
-
-
-
-
-
-//2
-
-function isGuessingGame2(quest1){
-	const option  = `
-		
-B. Which company won a $10 billion U.S. government contract for data analytics?
-	1) IBM
-	2) Palantir
-	3) Oracle
-	4) Cisco
-
-
-	`;
-	
-
-//✅ Answer: B) Palantir
-console.log(option);
-	switch(quest1){
-	case 1: console.log("1  Is incorrect, \n move to the next question"); break;
-	case 2: console.log("2 Is correct, \n move to the next question" ); break;
-	case 3: console.log("3 Is incorrect, \n move to the next question" ); break;
-	case 4: console.log("4 Is incorrect, \n move to the next question" ); break;
-		default : console.log(" Not a correct input" ); 
-	}
-return option;
-}
-
-//console.log( isGuessingGame2(4));
-
-
-//3
-function isGuessingGame3(quest3){
-	const option  = `
-		
+    case 3:
+      console.log(`
 3. What is the main reason for the Nasdaq’s recent all-time high?
-	A) Oil prices
-	B) AI boom
-	C) Crypto rally
-	D) Housing market
-
-	`;
+  1. Oil prices
+  2. AI boom
+  3. Crypto rally
+  4. Housing market
+`);
 	
-
-//✅ Answer: B) AI boom
-console.log(option);
-	switch(quest3){
-	case 1: console.log("1  Is incorrect, \n move to the next question"); break;
-	case 2: console.log("2 Is correct, \n move to the next question" ); break;
-	case 3: console.log("3 Is incorrect, \n move to the next question" ); break;
-	case 4: console.log("4 Is incorrect, \n move to the next question" ); break;
-		default : console.log(" Not a correct input" ); 
-	}
-return option;
-}
-
-//console.log( isGuessingGame3(4));
-
-
-
-//4
-function isGuessingGame4(quest1){
-	const option  = `
 		
+      rl.question("Your answer: ", (answer) => {
+        if (parseInt(answer) === 2) {
+        correct +=1
+          console.log(" Correct!.  Move to the next");
+        } else {
+         incorrect += 1
+          console.log(" Incorrect.  Move to the next");
+        }
+       presentQuestion++;
+        askQuestion();
+      });
+      break;
+
+    case 4:
+      console.log(`
 4. When is the nomination deadline for the TechRadar Choice Awards 2025?
-A) August 10
-B) August 15
-C) August 22
-D) August 31
+  1. August 10
+  2. August 15
+  3. August 22
+  4. August 31
+`);
+      rl.question("Your answer: ", (answer) => {
+        if (parseInt(answer) === 3) {
+        correct += 1
+          console.log("Correct!.  Move to the next");
+        } else {
+        	incorrect += 1
+          console.log(" Incorrect.  Move to the next");
+        }
+        presentQuestion++;
+        askQuestion();
+      });
+      break;
 
-
-
-	`;
-	
-
-//✅ Answer: C) August 22
-console.log(option);
-	switch(quest1){
-	case 1: console.log("1  Is incorrect, \n move to the next question"); break;
-	case 2: console.log("2 Is incorrect, \n move to the next question" ); break;
-	case 3: console.log("3 Is correct, \n move to the next question" ); break;
-	case 4: console.log("4 Is incorrect, \n move to the next question" ); break;
-		default : console.log(" Not a correct input" ); 
-	}
-return option;
-}
-
-//console.log( isGuessingGame4(3));
-
-
-
-//5
-
-function isGuessingGame5(quest5){
-	const option5 = `
-		
+    case 5:
+      console.log(`
 5. Which African city was ranked the top emerging tech hub in 2025?
-A) Nairobi
-B) Accra
-C) Johannesburg
-D) Lagos
+  1. Nairobi
+  2. Accra
+  3. Johannesburg
+  4. Lagos
+`);
 
-		`;
+      rl.question("Your answer: ", (answer) => {
+        if (parseInt(answer) === 4) {
+         correct += 1 
+          console.log("Correct!.  Move to the next");
+        } else {
+         incorrect += 1
+          console.log(" Incorrect. Move to the next");
+        }
+        console.log(`\n Quiz complete. you  got  ${correct} out of 5 `);
+        rl.close();
+      });
+      break;
+    default: console.log(" invalid input");
+    
 
-//✅ Answer: D) Lagos
-console.log(option5);
-	switch(quest5){
-	case 1: console.log("1  Is incorrect, \n move to the next question" ); break;
-	case 2: console.log("2 Is incorrect, \n move to the next question" ); break;
-	case 3: console.log("3 Is incorrect, \n move to the next question" ); break;
-	case 4: console.log("4 Is correct, \n move to the next question" ); break;
-		default : console.log(" Not a correct input" ); 
-	}
-return option5;
+  }
 }
 
-//console.log(isGuessingGame5(3));
