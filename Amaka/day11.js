33d0068d9d369972b4d978fcc9e7becbf5177011
Chiamie <okechukwurchiamaka@gmail.com>
2025-08-14 06:26:45 +0100

    updated and done

--- Amaka/day11.js
+++ Amaka/day11.js
@@ -3,22 +3,31 @@ function getTheHighestAndLowestOf(stringOfNmbers){
 	let arrayOfNumbers = arrayOfStrings.map(Number);
 	let highest = 0;
 	let lowest = Infinity;
-	console.log(arrayOfNumbers);
 
 	for(let index = 0; index < arrayOfNumbers.length; index++){
 		if (arrayOfNumbers[index] > highest)
 			highest = arrayOfNumbers[index];
-			console.log(highest)
 		if (arrayOfNumbers[index] < lowest)
 			lowest = arrayOfNumbers[index];
 	}
-	console.log(highest, lowest);
-	return arrayOfNumbers.filter(number => number === highest || number === lowest);
+	let result = arrayOfNumbers.filter(number => number === highest || number === lowest);
+	return `${result.join(" ")}`;
+
 }
 
 
 let input = "1 2 3 4 5"
 console.log(getTheHighestAndLowestOf(input));
+
+let input1 = "1 9 3 4 -5"
+console.log(getTheHighestAndLowestOf(input1));
+
+
+let input2 = "1 2 -3 4 5"
+console.log(getTheHighestAndLowestOf(input2));
+
+
+
 
 
 
