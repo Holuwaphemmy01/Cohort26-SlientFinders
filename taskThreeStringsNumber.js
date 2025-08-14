b19735f9081dc615487c93bc63412a20dbfe9887
Ejiofor samuel chukwunonso <samuelchukwusom360@gmail.com>
2025-08-14 16:30:11 +0100

    taskThreeDone

---
+++ taskThreeStringsNumber.js
@@ -0,0 +1,9 @@
+function lengthOfStrings(strings){
+
+return strings.map((element)=> element.length);
+
+
+}
+
+newStrings = ["Sam","Basit","Emmanuel", "Fareed"];
+console.log(lengthOfStrings(newStrings));
