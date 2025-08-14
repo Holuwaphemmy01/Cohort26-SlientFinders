9d42f07a9082140111af865afe92a0d7e7a60ba2
Delight44-max <eboagorojahdiebube@gmail.com>
2025-08-14 16:01:40 +0100

    just added one file

---
+++ Delight/Object.js
@@ -0,0 +1,9 @@
+let person = { name: "Delight",
+             age: 25,
+             Gender: "male",
+             City: "Lagos"
+             }
+for(let property in person) {
+console.log(` ${property}: ${person[property]}`)
+}
+
