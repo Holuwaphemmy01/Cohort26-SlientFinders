588cfba49e383129487ca64478b708543b0046d2
Hunterly01 <mhunterly@gmail.com>
2025-08-14 17:30:38 +0100

    add

--- Abdulrahman/10 codes/Task4.js
+++ Abdulrahman/10 codes/Task4.js
@@ -1,7 +1,12 @@
-let studentfile = [{name: "Hunter", score: 67}, {name: "Mtee", score: 50}, {name: "ibrahim", score: 90}]
-
-for(let studentDetail in studentfile){
-	for(let score in studentDetail){
-
-
+function getHighestStudentScore(studentScore){
+let Highest = studentScore[0]
+for(let count = 1; count < studentScore.length; count++){
+	if(studentScore[count].score > Highest.score){
+	Highest = studentScore[count];
 }
+}
+ return Highest;
+}
+
+let studentfile = [{name: "Hunter", score: 67}, {name: "Mtee", score: 50}, {name: "ibrahim", score: 90}]
+console.log(getHighestStudentScore(studentfile))
