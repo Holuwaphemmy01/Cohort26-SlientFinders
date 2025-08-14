938b872a80d240805e950fb40a62fc73ff3439f4
Ejiofor samuel chukwunonso <samuelchukwusom360@gmail.com>
2025-08-14 19:00:32 +0100

    made some changes

--- TaskSixColourIndex.js
+++ TaskSixColourIndex.js
@@ -2,17 +2,18 @@ function objectLength(colours){
 result = colours.map((elements)=> elements.length);
 
 
-let a = colours[0];
+let newArray = [];
 let object = {};
 for (let count = 0; count < colours.length; count++){
 object[colours[count]] = result[count];
-
-}
-return  object;
 }
 
+newArray.push(object);
+return newArray;
+}
 
 
 
-let colors = ["red", "green", "blue"];
+
+let colors = ["red", "green", "blue" , "yellow"];
 console.log(objectLength(colors));
