5bee50bdede2f86608f43de0ca216962c4ceb7aa
EngrEOOnoja <onojaemmanuel193@gmail.com>
2025-08-14 15:17:24 +0100

    Lunch Progress

---
+++ OnojaEmmanuelJS/LUNCH/objectLoop.js
@@ -0,0 +1,9 @@
+let person = {Name : "Onoja Emmanuel" ,
+			Age	 : 10,
+		Gender  : "Male",
+		City : "Spain"
+								}
+	for (let quality in person){
+	console.log(`${quality}: ${person[quality]}`)
+	}
+	
