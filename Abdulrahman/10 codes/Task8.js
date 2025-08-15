25c3d6b3a8863d48b53f34552bc112568198358c
Hunterly01 <mhunterly@gmail.com>
2025-08-15 08:17:17 +0100

    done

--- Abdulrahman/10 codes/Task8.js
+++ Abdulrahman/10 codes/Task8.js
@@ -1,6 +1,9 @@
 const users = [{name: "Alice", age: 25}, {name: "Bob", age: 17}, {name: "Charlie", age: 30}];
-for(let count = 1; count < users.length; count++){
-if(users[count].age > 18){
-console.log(users[count])
+let newArray = [];
+for(let count = 0; count < users.length; count++){
+if(users[count].age >= 18){
+newArray.push(users[count]);
+
 }
 }
+console.log(newArray)
