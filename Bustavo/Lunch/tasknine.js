08d28678c261e126c2ba10ae018ad523f571c593
Bustavo-coder <abdbasitegberongbe@gmail.com>
2025-08-16 13:37:52 +0100

    Everything is possible In coding

--- Bustavo/Lunch/tasknine.js
+++ Bustavo/Lunch/tasknine.js
@@ -1,10 +1,25 @@
 //Write a function that counts how many times each elements appears in an array and returns an object with element counts
+function countDuplicate(array){
+	let countingAmountArray = []
+	let obj = {}
+		for(let count = 0; count < array.length;count++){
+				sum = 0
+				if (!(countingAmountArray.includes(array[count]))){
+					countingAmountArray.push(array[count])
 
-arr = [1,2,3,4,5,1,1,4,5,1,7,8,9,1,2,4,5,4]
-for(let count = 0;count < arr.length;count++){
-		let sum = 0
-	for(let counter = 0; counter < arr.length;counter++){
-		if(arr[count] == arr[counter]) sum++
-		}
-		console.log(`${arr[count]} is ${sum}`)
-	}
+				for(let counter = 0; counter < array.length;counter++){
+					if(array[count] == array[counter]) sum++
+					}
+					obj[array[count]] = sum
+					}
+				
+			}
+		
+
+		return obj
+}
+
+let arra = [1,2,3,4,5,6,7,1,7,2,3,4,5,6,1,8]
+let arrangeNane = [1,2,3,4,5,1,1,4,5,1,7,8,9,1,2,4,5,4]
+console.log(countDuplicate(arra))
+
