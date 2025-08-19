119a5ecb3f83bab101335d2d243f2cb41c312461
Silver36-ship-it <emycallistus@gmail.com>
2025-08-19 05:29:07 +0100

    Today task

---
+++ Silver/Day16/array_of_integers.js
@@ -0,0 +1,10 @@
+let number = 5
+new_array = []
+for(let index = number; index >= 1; index--){
+if(number <= 0){
+console.log("Invalid input") 
+}
+else
+new_array.push(index)
+}
+console.log(new_array)
--- Silver/Lunch Task/taskFour.js
+++ Silver/Lunch Task/taskFour.js
@@ -1,14 +1,21 @@
 let students = [{Name : 'Ade',
-			Score: 32},
+			Score: 32,
+			height: 5},
 			{Name: 'Jude',
-			Score: 21},
+			Score: 21,
+			height: 9},
 			{Name: 'David',
-			Score: 23},
+			Score: 23,
+			height: 98},
 			{Name: 'Kate',
-			Score:87}]
+			Score:87,
+			height: 27}]
+highest_studentScore = students[0]
 for(const properties in students){
-if (students[properties].Score === 21){
-console.log(`${properties}:${students[properties]}`)
+console.log(students[properties].Score,students[properties].Name,properties,students[properties].height)
+if (students[properties].Score > highest_studentScore.Score){
+	highest_studentScore = students[properties]
+console.log(`Highest student: ${highest_studentScore.Name}, Score :${highest_studentScore.Score}`)
 }
 }
 			
