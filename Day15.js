772463d8cb1ec01cfed4780c9269b10a8396c359
FiyinfoluwaDK <106954393+FiyinfoluwaDK@users.noreply.github.com>
2025-08-19 08:43:37 +0100

    assignment

---
+++ Day15.js
@@ -0,0 +1,17 @@
+function countSheep(sheep) {
+  let count = 0;
+  for (let i = 0; i < sheep.length; i++) {
+    if (sheep[i] === true) {
+      count++;
+    }
+  }
+  return count;
+}
+
+const sheepArray = [
+  true, true, true, false, true, true, true, true,
+  true, false, true, false, true, false, false, true,
+  true, true, true, true, false, false, true, true
+];
+
+console.log("Number of sheep present:", countSheep(sheepArray));
