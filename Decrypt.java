3010c4711b77cbc7615eb4023c3ed165225cfe5e
SadiqIbrahim01 <164149043+SadiqIbrahim01@users.noreply.github.com>
2025-08-18 19:02:28 -0700

    Add files via upload

---
+++ Decrypt.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class Decrypt {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.print("Enter an encrypted four-digit integer: ");
+        int encrypted = scanner.nextInt();
+        
+        int d1 = encrypted / 1000;
+        int d2 = (encrypted / 100) % 10;
+        int d3 = (encrypted / 10) % 10;
+        int d4 = encrypted % 10;
+        
+        int temp = d1;
+        d1 = d3;
+        d3 = temp;
+        
+        temp = d2;
+        d2 = d4;
+        d4 = temp;
+        
+        d1 = (d1 + 3) % 10;
+        d2 = (d2 + 3) % 10;
+        d3 = (d3 + 3) % 10;
+        d4 = (d4 + 3) % 10;
+        
+        int original = d1 * 1000 + d2 * 100 + d3 * 10 + d4;
+        
+        System.out.println("Original number: " + original);
+        
+     
+    }
+}
