9ae9ee3b80d49a5d7dfb2904b6ce181964545842
Chiamie <okechukwurchiamaka@gmail.com>
2025-08-31 20:23:26 +0100

    modified the function to handle a bug that was identified

--- Amaka/day20.js
+++ Amaka/day20.js
@@ -1,14 +1,30 @@
-
-
-
-function getInitialsOf(sentence){
+/**function getInitialsOf(sentence){
 	let initials = sentence.charAt(0);
 	initials += '.';
 	initials += sentence.charAt((sentence.indexOf(" ") + 1));
 	return initials;
 }
 
-console.log(getInitialsOf("Sam Harris"));
+**/
 
+
+function getInitialsOf(sentence){
+	let array  = sentence.split(" ");
+	let initials = "";
+	for(let index = 0; index < array.length; index++){
+		initials += array[index].charAt(0).toUpperCase();
+		if (index == array.length - 1)
+			break;
+		initials += ".";
+	}
+		
+		
+		
+	return initials;
+}
+
+console.log(getInitialsOf("Sam Harris"));
+console.log(getInitialsOf("Sam Harris Enugu"));
+console.log(getInitialsOf("peter kingsley"));
 
 
